33c02d81f62e00b9beac5a408aaeeabb15091f20
mariem <sm.mariem99@gmail.com>
2022-07-21 10:45:57 +0100

    hooks

--- mariem-blog/src/Home.js
+++ mariem-blog/src/Home.js
@@ -1,9 +1,46 @@
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
 const Home = () => {
-    return ( 
-        <div className="home">
-            <h2>Home page</h2>
-        </div>
-     );
-}
- 
+  const [blogs, setBlogs] = useState([
+    {
+      title: "my website",
+      body: "hello and welcome here",
+      author: "mario",
+      id: 1,
+    },
+    {
+      title: "web developement tips",
+      body: "hello here",
+      author: "max",
+      id: 2,
+    },
+    {
+      title: "Web developement website",
+      body: "welcome here",
+      author: "maria",
+      id: 3,
+    },
+  ]);
+  const [name, setName] = useState("mariam");
+  //DELETE BUTTON
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+  useEffect(() => {
+    console.log("useffect run !");
+  }, [name]);
+  return (
+    <div className="home">
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <BlogList
+        blogs={blogs.filter((blog) => blog.author === "mario")}
+        title="Mario's blogs"
+      />
+      <button onClick={() => setName("eya")}>change name</button>
+      <p>{name}</p>
+    </div>
+  );
+};
+
 export default Home;
--- mariem-blog/src/Navbar.js
+++ mariem-blog/src/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
     <h1> Mariem 's blog</h1>
     <div className="links">
         <a href="/">Home</a>
-        <a href="/create">New Blog</a>
+        <a href="/create" >New Blog</a>
 
     </div>
    </nav>
